700d3d7d34116e3d2bde50dc678db071b31ad2d8
Gerardo Santana <91979984+JerrySantana@users.noreply.github.com>
2023-11-04 19:55:30 -0600

    Added some css configurations

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  const p1 = <p> Hello {value} </p>;
-  const titles = <title> Image analysis and generation. </title>;
-  const tb1 = <textarea> Enter your image url here: </textarea>;
-  const btnIa = <button> Image analysis </button>;
-  const btnIg = <button> Image generation </button>;
-  const page = <div> titles p1 tb1 btnIa btnIg </div>;
+  const p1 = <p id='hello'> Hello {value} </p>;
+  const titles = <title id='title'> Image analysis and generation. </title>;
+  const tb1 = <textarea id='textBox'> Enter your image url here: </textarea>;
+  const btnIa = <button id='imA'> Image analysis </button>;
+  const btnIg = <button id='imG'> Image generation </button>;
+  const page = <div className='page'>{titles} {p1} {tb1} {btnIa} {btnIg}</div>;
   
   return page;
 }
