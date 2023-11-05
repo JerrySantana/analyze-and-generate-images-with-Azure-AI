cb60b3c805112541ff14d09ed0398935d3f3fdbc
Gerardo Santana <91979984+JerrySantana@users.noreply.github.com>
2023-11-04 20:29:29 -0600

    Modified the css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 function App() {
   const value = 'World';
+
+  const titles = <title> Image analysis and generation. </title>;
+//  const css1 = <link rel='stylesheet' href='index.css' />;
+
   const p1 = <p id='hello'> Hello {value} </p>;
-  const titles = <title id='title'> Image analysis and generation. </title>;
-  const tb1 = <textarea id='textBox'> Enter your image url here: </textarea>;
-  const btnIa = <button id='imA'> Image analysis </button>;
-  const btnIg = <button id='imG'> Image generation </button>;
-  const page = <div className='page'>{titles} {p1} {tb1} {btnIa} {btnIg}</div>;
+  const tb1 = <textarea id='textBox' name='textBox' placeholder='Enter your image url here:'></textarea>;
+  const btnIa = <button name='imA'> Image analysis </button>;
+  const btnIg = <button name='imG'> Image generation </button>;
+
+  const buttons = <div className='buttons'>{btnIa} {btnIg}</div>;
+  const head1 = <head>{titles}</head>;
+  const body1 = <body> <div className='page'>{titles} {p1} {tb1} {buttons}</div> </body>;
+  
+  const page = <html>{head1} {body1}</html>;
   
   return page;
 }
