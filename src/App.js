c02b14270d1abf4c611d657c4a776f1d5c2064c7
Gerardo Santana <91979984+JerrySantana@users.noreply.github.com>
2023-11-04 20:37:06 -0600

    Changed css and html

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-
-  const titles = <title> Image analysis and generation. </title>;
-//  const css1 = <link rel='stylesheet' href='index.css' />;
-
   const p1 = <p id='hello'> Hello {value} </p>;
   const tb1 = <textarea id='textBox' name='textBox' placeholder='Enter your image url here:'></textarea>;
   const btnIa = <button name='imA'> Image analysis </button>;
   const btnIg = <button name='imG'> Image generation </button>;
 
   const buttons = <div className='buttons'>{btnIa} {btnIg}</div>;
-  const head1 = <head>{titles}</head>;
   const body1 = <body> <div className='page'>{titles} {p1} {tb1} {buttons}</div> </body>;
   
-  const page = <html>{head1} {body1}</html>;
+  const page = <html>{body1}</html>;
   
   return page;
 }
