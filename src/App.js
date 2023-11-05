ec5922aa9fc11e139d62a96e93fc7ed6c2dea791
Gerardo Santana <91979984+JerrySantana@users.noreply.github.com>
2023-11-04 19:26:21 -0600

    Added the title, a text box and two buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function App() {
-  const value = 'Jerry';
-  return <div>Hello {value}</div>;
+  const value = 'World';
+  const div1 = <div> 'Hello {value}' </div>;
+  const titles = <title> 'Image analysis and generation.' </title>;
+  const tb1 = <textarea> 'Enter your image url here:' </textarea>;
+  const btnIa = <button> 'Image analysis' </button>;
+  const btnIg = <button> 'Image generation' </button>;
+  const page = [titles, div1, tb1, btnIa, btnIg];
+  
+  return page;
 }
 
 export default App;
